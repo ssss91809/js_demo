6340bc802b143cac05b1e6a8b911b022a079a3aa
steven <ssss91809@gmail.com>
2024-01-18 20:28:16 +0800

    樂透

---
+++ js.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    let rows =[]
+    for ( let =j; j>5; j++){
+    rows.push(j);
+    }
+
+</body>
+
+</html>
